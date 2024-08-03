9e4113cdd71758143f0ff7b9fd2fc7689bce9cf0
C4 Patino <c4patino@gmail.com>
2024-08-03 14:00:18 -0500

    Whoops

---
+++ hosts/kokoro/default.nix
@@ -0,0 +1,109 @@
+{ config, pkgs, inputs, ... }:
+{
+    imports = [
+        ./hardware-configuration.nix
+        ../../nixosModules
+    ];
+
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    # Bootloader.
+    boot.loader = {
+        efi.canTouchEfiVariables = true;
+        grub = {
+            enable = true;
+            devices = [ "nodev" ];
+            efiSupport = true;
+            useOSProber = true;
+        };
+    };
+
+    networking.hostName = "arisu"; # Define your hostname.
+    networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+    
+    # Enable bluetooth
+    hardware.bluetooth.enable = true;
+    hardware.bluetooth.powerOnBoot = true;
+    services.blueman.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "America/Chicago";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+    };
+
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    services.xserver.displayManager.gdm = {
+        enable = true;
+    };
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+    };
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    security.polkit.enable = true;
+
+    # Enable sound with pipewire.
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        wireplumber.enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+    };
+
+    # Define a user account.
+    users.users.c4patino = {
+        isNormalUser = true;
+        description = "C4 Patino";
+        extraGroups = [ "networkmanager" "wheel" "vboxusers" "docker" ];
+        shell = pkgs.zsh;
+    };
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # List packages installed in system profile. To search, run:
+    environment.systemPackages = with pkgs; [ 
+        home-manager 
+
+        networkmanagerapplet
+    ];
+
+    programs.zsh.enable = true;
+
+    programs.nix-ld = {
+        enable = true;
+        libraries = with pkgs; [
+            stdenv.cc.cc
+            cudaPackages.cudatoolkit
+        ];
+    };
+
+    system.stateVersion = "24.05";
+}
