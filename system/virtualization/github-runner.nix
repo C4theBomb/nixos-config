25d0b438af261d0621e6087126a7136c31872c2f
C4 Patino <c4patino@gmail.com>
2024-09-03 16:17:26 -0500

    Fixed problems with runners booting before auth available

--- system/virtualization/github-runner.nix
+++ system/virtualization/github-runner.nix
@@ -8,6 +8,9 @@
     };
 
     config = lib.mkIf config.github-runners.enable {
+        systemd.user.services.github-runner-arisu.after = [ "sops-nix.service" ];
+        # systemd.user.services.github-runner-arisu-oasys.after = [ "sops-nix.service" ];
+
         services.github-runners = {
             arisu = {
                 enable = true;
@@ -15,6 +18,12 @@
                 tokenFile = /home/c4patino/.config/sops-nix/secrets/github-runner;
                 url = "https://github.com/C4theBomb/nixos-config";
             };
+            # arisu-oasys = {
+            #     enable = true;
+            #     ephemeral = true;
+            #     tokenFile = /home/c4patino/.config/sops-nix/secrets/github-runner;
+            #     url = "https://github.com/oasys-mas";
+            # };
         };
     };
 }
