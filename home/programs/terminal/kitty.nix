224bf9c4abd70b78a253aae346da28627bef33f3
C4 Patino <c4patino@gmail.com>
2024-10-04 08:52:15 -0500

    Added tokyonight theme

--- home/programs/terminal/kitty.nix
+++ home/programs/terminal/kitty.nix
@@ -12,6 +12,7 @@
             enable = true;
             shellIntegration.enableZshIntegration = true;
             font.name = "MesloLGM Nerd Font Mono";
+			themeFile = "tokyo_night_night";
 
             keybindings = {
                 "ctrl+enter" = "new_window_with_cwd";
