56ee5925eb5162677509b97a2ae6348cee4eb4ad
C4 Patino <c4patino@gmail.com>
2024-10-04 21:07:10 -0500

    Added other syncthing ids

--- system/services/syncthing.nix
+++ system/services/syncthing.nix
@@ -16,6 +16,14 @@
 						id = "DRHDM4H-V3PNL37-XHUZNKB-L5OSHLY-HPZEKTW-ANIPKHM-IJHLAIO-YTLAMQJ";
 						autoAcceptFolders = true;
 					};
+					"kokoro" = {
+						id = "GNLFB3X-TFDTLJY-NSZWWUT-JOWSBFQ-DXLZA4E-OTXL2SD-GNUQBY3-3R6AMAB";
+						autoAcceptFolders = true;
+					};
+					"chibi" = {
+						id = "QV2Y5R5-3RHJDCW-L5L633X-UMASLYU-SASAOPE-CSCYPVH-XPZSWQX-UZEEJA7";
+						autoAcceptFolders = true;
+					};
 				};
 				folders = {
 				};
