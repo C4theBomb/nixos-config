b129f060317f3fb5824e9067f2092333d67e03fd
C4 Patino <c4patino@gmail.com>
2024-10-04 19:50:20 -0500

    Updated with final slurm config

--- system/services/slurm.nix
+++ system/services/slurm.nix
@@ -1,4 +1,4 @@
-{ lib, config, self, inputs, ... }:
+{ lib, config, self, inputs, pkgs, ... }:
 let
 	primaryHost = "arisu";
 	computeNodes = [ "arisu" "kokoro" "chibi" ];
@@ -20,32 +20,36 @@ in
 			client.enable = builtins.elem config.networking.hostName computeNodes;
 			server.enable = (config.networking.hostName == primaryHost);
 
-			dbdserver = {
-				enable = (config.networking.hostName == primaryHost);
-				dbdHost = primaryHost;
-			};
-
             nodeName = [ 
-				"arisu NodeAddr=100.89.24.88 CPUs=12 Sockets=1 CoresPerSocket=6 ThreadsPerCore=2 RealMemory=63443 Gres=gpu:1,shard:12 Weight=100 State=UNKNOWN" 
-				"kokoro NodeAddr=100.126.34.64 CPUs=10 Sockets=1 CoresPerSocket=10 ThreadsPerCore=1 RealMemory=23746 Weight=10 State=UNKNOWN" 
-				"chibi NodeAddr=100.101.224.25 CPUs=4 Sockets=1 CoresPerSocket=4 ThreadsPerCore=1 RealMemory=7750 Weight=50 State=UNKNOWN"
+				"arisu NodeAddr=100.89.24.88 CPUs=12 Sockets=1 CoresPerSocket=6 ThreadsPerCore=2 RealMemory=63443 Gres=gpu:1,shard:12 Weight=1 State=UNKNOWN"
+				"kokoro NodeAddr=100.126.34.64 CPUs=10 Sockets=1 CoresPerSocket=10 ThreadsPerCore=1 RealMemory=23746 Weight=100 State=UNKNOWN"
+				"chibi NodeAddr=100.101.224.25 CPUs=4 Sockets=1 CoresPerSocket=4 ThreadsPerCore=1 RealMemory=7750 Weight=10 State=UNKNOWN"
 			];
 
             partitionName = [ 
                 "main Nodes=arisu,chibi Default=YES MaxTime=INFINITE State=UP"
-                "extended Nodes=arisu,kokoro,chibi Default=YES MaxTime=INFINITE State=UP"
+                "extended Nodes=arisu,kokoro,chibi Default=NO MaxTime=INFINITE State=UP"
             ];
 
 			extraConfig = ''
-				TaskPlugin=task/cgroup
 				GresTypes=gpu,shard
+				TaskPlugin=task/cgroup
 				SlurmdParameters=allow_ecores
 				DefCpuPerGPU=1
 				DefMemPerCPU=1000
 				ReturnToService=2
+
+				AccountingStorageType=accounting_storage/slurmdbd
+				JobAcctGatherType=jobacct_gather/cgroup
 			'';
 
 			extraConfigPaths = [ (inputs.dotfiles + "/slurm") ];
+
+			dbdserver = {
+				enable = (config.networking.hostName == primaryHost);
+				dbdHost = primaryHost;
+				storagePassFile = "${self}/secrets/crypt/mysql.txt";
+			};
         };
 
         services.munge.enable = true;
@@ -55,5 +59,12 @@ in
             group = "munge";
             mode = "0400";
         };
+
+		services.mysql = {
+			enable = (config.networking.hostName == primaryHost);
+			initialDatabases = [ { name = "slurm_acct"; } ];
+			ensureDatabases = [ "slurm_acct" ];
+			package = pkgs.mariadb;
+		};
     };
 }
