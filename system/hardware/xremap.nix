4588a9107e401d7b5aec450a3b817d214ec2a83a
C4 Patino <c4patino@gmail.com>
2024-10-06 20:54:39 -0500

    Fixed remap configuration

--- system/hardware/xremap.nix
+++ system/hardware/xremap.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, inputs, ... }: {
+{ config, lib, inputs, ... }: {
     options = {
         xremap.enable = lib.mkOption {
             type = lib.types.bool;
@@ -6,29 +6,27 @@
             description = "Enable Xremap and keybind remaps";
         };
     };
-    
-    imports = [
-        inputs.xremap.nixosModules.default
-    ];
+
+	imports = [
+		inputs.xremap.nixosModules.default
+	];
 
     config = {
         services.xremap = {
+			enable = config.xremap.enable;
             userName = "c4patino";
-            config.modmap = if config.xremap.enable then
-                [
-                    {
-                        name = "Main editing remaps";
-                            remap = {
-                                CAPSLOCK = {
-                                held = "CONTROL_L";
-                                alone = "ESC";
-                                alone_timeout_millis = 250;
-                            };
-                        };
-                    }
-                ]
-                else
-                [];
+            config.modmap = [
+				{
+					name = "Main editing remaps";
+						remap = {
+							CAPSLOCK = {
+							held = "CONTROL_L";
+							alone = "ESC";
+							alone_timeout_millis = 250;
+						};
+					};
+				}
+			];
         };
     };
 }
