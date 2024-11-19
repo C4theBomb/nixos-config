15cfdd15ab1baf5c8126e87c35d235fbf73be647
C4 Patino <c4patino@gmail.com>
2024-11-18 22:24:00 -0600

    Added automatic resurrection to pm2 configuration

--- system/services/pm2.nix
+++ system/services/pm2.nix
@@ -16,5 +16,19 @@
     environment.systemPackages = with pkgs; [
       pm2
     ];
+
+    systemd.services.pm2 = {
+      enable = true;
+      description = "pm2";
+      unitConfig = {
+        Type = "simple";
+      };
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.pm2}/bin/pm2 resurrect";
+        ExecReload = "${pkgs.pm2}/bin/pm2 reload all";
+        ExecStop = "${pkgs.pm2}/bin/pm2 kill";
+      };
+    };
   };
 }
