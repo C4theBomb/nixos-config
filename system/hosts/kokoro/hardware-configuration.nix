25b9f10736d83084f57783b4f799a99093856ac9
C4 Patino <c4patino@gmail.com>
2024-11-19 14:03:33 -0600

    Added lid switch ignore for external power

--- system/hosts/kokoro/hardware-configuration.nix
+++ system/hosts/kokoro/hardware-configuration.nix
@@ -4,7 +4,6 @@
 {
   config,
   lib,
-  pkgs,
   modulesPath,
   ...
 }: {
@@ -14,6 +13,8 @@
   boot.initrd.kernelModules = ["dm-snapshot"];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
+
+  services.logind.lidSwitchExternalPower = "ignore";
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
