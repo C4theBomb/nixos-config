2fa8b2119ba35b5549462025df1c29f5b14bbd45
C4 Patino <c4patino@gmail.com>
2024-12-01 20:14:28 -0600

    Added zig development environment

---
+++ envs/zig.nix
@@ -0,0 +1,25 @@
+{
+  pkgs ?
+    import <nixpkgs> {
+      config = {
+        allowUnfree = true;
+        cudaSupport = true;
+      };
+    },
+}:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    zig
+    zls
+    lldb
+  ];
+
+  shell = pkgs.zsh;
+
+  shellHook = ''
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.libGL}/lib
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.glib.out}/lib
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/run/opengl-driver/lib
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NIX_LD_LIBRARY_PATH;
+  '';
+}
