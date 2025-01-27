1a6a9f3aae2dc75a58e47d5f4e285839dcf6e235
C4 Patino <c4patino@gmail.com>
2025-01-26 23:00:41 -0600

    Reformatted home configuration settings

--- home/profiles/default.nix
+++ home/profiles/default.nix
@@ -27,24 +27,21 @@
   secrets = builtins.fromJSON (builtins.readFile "${self}/secrets/crypt/secrets.json");
 in {
   _module.args = {inherit homeImports;};
-
-  flake = {
-    homeConfigurations = {
-      "c4patino@arisu" = inputs.home-manager.lib.homeManagerConfiguration {
-        modules = homeImports."c4patino@arisu";
-        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-        inherit extraSpecialArgs;
-      };
-      "c4patino@kokoro" = inputs.home-manager.lib.homeManagerConfiguration {
-        modules = homeImports."c4patino@kokoro";
-        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-        inherit extraSpecialArgs;
-      };
-      "c4patino@chibi" = inputs.home-manager.lib.homeManagerConfiguration {
-        modules = homeImports."c4patino@chibi";
-        pkgs = inputs.nixpkgs.legacyPackages.aarch64-linux;
-        inherit extraSpecialArgs;
-      };
+  flake.homeConfigurations = {
+    "c4patino@arisu" = inputs.home-manager.lib.homeManagerConfiguration {
+      modules = homeImports."c4patino@arisu";
+      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+      inherit extraSpecialArgs;
+    };
+    "c4patino@kokoro" = inputs.home-manager.lib.homeManagerConfiguration {
+      modules = homeImports."c4patino@kokoro";
+      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+      inherit extraSpecialArgs;
+    };
+    "c4patino@chibi" = inputs.home-manager.lib.homeManagerConfiguration {
+      modules = homeImports."c4patino@chibi";
+      pkgs = inputs.nixpkgs.legacyPackages.aarch64-linux;
+      inherit extraSpecialArgs;
     };
   };
 }
