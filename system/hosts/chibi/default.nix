f483d3de723602ef5b8d111f83a8ee1f47661c52
C4 Patino <c4patino@gmail.com>
2025-01-30 20:36:31 -0600

    Updated chibi configuration to function properly on installation

--- system/hosts/chibi/default.nix
+++ system/hosts/chibi/default.nix
@@ -1,4 +1,5 @@
 {
+  self,
   pkgs,
   inputs,
   ...
@@ -6,8 +7,6 @@
   imports = [
     ../..
     ./hardware-configuration.nix
-
-    inputs.nixos-hardware.nixosModules.raspberry-pi-4
 
     inputs.disko.nixosModules.default
     (import ./disko.nix {
@@ -25,12 +24,19 @@
     isNormalUser = true;
     description = "C4 Patino";
     extraGroups = ["networkmanager" "wheel" "vboxusers" "docker" "podman" "syncthing"];
+
     hashedPassword = "$6$XM5h391mH33WIoAy$xkeSzw/ootPPZbvHEqSguZDyB4gAeTMcjy1aRXcXcQWFkS1/SRPK27VgEYC.vYvdZLYWALZtpdEzWAfwT4VCM1";
+
+    openssh.authorizedKeys.keyFiles = [
+      "${self}/secrets/crypt/ssh/arisu/id_ed25519.pub"
+      "${self}/secrets/crypt/ssh/kokoro/id_ed25519.pub"
+      "${self}/secrets/crypt/ssh/chibi/id_ed25519.pub"
+    ];
+
     shell = pkgs.zsh;
   };
 
   efi-bootloader.enable = true;
-  boot.loader.generic-extlinux-compatible.enable = false;
 
   network-manager.enable = true;
 
